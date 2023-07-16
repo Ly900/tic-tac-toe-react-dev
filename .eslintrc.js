e3ab89107a6cca2b0c5123624fbd9bb05b30382f
Ly Nguyen <lynguyendc@gmail.com>
2023-07-16 17:40:07 -0400

    Single quotes in JS files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,26 @@ module.exports = {
 		browser: true,
 		es2021: true,
 	},
-	extends: ["eslint:recommended", "plugin:react/recommended"],
+	extends: ['eslint:recommended', 'plugin:react/recommended'],
 	overrides: [
 		{
 			env: {
 				node: true,
 			},
-			files: [".eslintrc.{js,cjs}"],
+			files: ['.eslintrc.{js,cjs}'],
 			parserOptions: {
-				sourceType: "script",
+				sourceType: 'script',
 			},
 		},
 	],
 	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
+		ecmaVersion: 'latest',
+		sourceType: 'module',
 	},
-	plugins: ["react"],
+	plugins: ['react'],
 	rules: {
-		"react/react-in-jsx-scope": "off",
-		"react/jsx-uses-react": "off",
-		"react/prop-types": "off",
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-uses-react': 'off',
+		'react/prop-types': 'off',
 	},
 };
