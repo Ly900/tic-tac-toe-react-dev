0cd1ca1fbc8022817395a73429f1ee1df535fcb5
Ly Nguyen <lynguyendc@gmail.com>
2023-07-27 17:42:29 -0400

    Implement jumping around in history

--- src/App.js
+++ src/App.js
@@ -57,16 +57,20 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
 	const [xIsNext, setXIsNext] = useState(true);
 	const [history, setHistory] = useState([Array(9).fill(null)]);
-
-	const currentSquares = history[history.length - 1];
+	const [currentMove, setCurrentMove] = useState(0);
+	const currentSquares = history[currentMove];
 
 	function handlePlay(nextSquares) {
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
 		setHistory([...history, nextSquares]);
+		setXIsNext(!xIsNext);
+		setCurrentMove(nextHistory.length - 1);
 		setXIsNext(!xIsNext);
 	}
 
 	function jumpTo(nextMove) {
-		// TODO
+		setCurrentMove(nextMove);
+		setXIsNext(nextMove % 2 === 0);
 	}
 
 	console.log(history);
